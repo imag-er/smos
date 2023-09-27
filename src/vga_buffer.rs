edc0adff127679adc9f3150edd17d82da9e2e6ad
imag-er <2274265784@qq.com>
2023-09-27 23:39:21 +0800

    first commit

---
+++ src/vga_buffer.rs
@@ -0,0 +1,116 @@
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+	Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+	fn new(foreground_color  : Color,background_color : Color) -> ColorCode {
+		ColorCode((background_color as u8) << 4 | (foreground_color as u8))
+
+	}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+	ascii_character : u8,
+	color_code : ColorCode,
+}
+
+const BUFFER_HEIGHT : usize = 25;
+const BUFFER_WIDTH : usize = 80;
+
+
+#[repr(transparent)]
+struct  Buffer {
+	// 二维数组定义
+	chars : [[ScreenChar; BUFFER_WIDTH];BUFFER_HEIGHT]
+}
+
+// 用于向buffer写入
+pub struct Writer {
+	row_position : usize,
+	colume_position : usize,
+	color_code: ColorCode,
+	// vga buffer全局有效
+	buffer: &'static mut Buffer,
+}
+
+
+impl Writer {
+	// 写入byte
+	pub fn write_byte(&mut self,byte: u8)
+	{
+		match byte {
+			b'\n' => self.new_line(),
+			b'\r' => self.colume_position = 0,
+			byte => {
+				if self.colume_position >= BUFFER_WIDTH{
+					// 超出行宽度 新行
+					self.new_line();
+				}
+				// let row = BUFFER_HEIGHT - 1;
+				let row = self.row_position;
+
+				let col = self.colume_position;
+
+				let color_code = self.color_code;
+
+				self.buffer.chars[row][col] = ScreenChar {
+					ascii_character : byte,
+					color_code
+				};
+
+				self.colume_position += 1;
+			}
+		}
+	}
+
+	pub fn write_string(&mut self,s: &str) {
+		for byte in s.bytes() {
+			match byte {
+				// 在CodePage437定义内的字符
+				0x20..=0x7E | b'\n' | b'\r' => self.write_byte(byte),
+				// 其他字符 打印方块 0xfe
+				_ => self.write_byte(0xfe),
+			}
+		}
+	}
+	fn new_line(&mut self) {
+		self.row_position += 1;
+		self.colume_position = 0;
+	}
+}
+pub fn print_test_msg()
+{
+	let mut vga_buffer_writer = Writer {
+		colume_position: 0,
+		row_position: 0,
+		color_code: ColorCode::new(Color::White, Color::Pink),
+		buffer: unsafe { &mut *(0xb8000 as *mut Buffer)}
+	};
+
+	vga_buffer_writer.write_string("Hello World\n123123");
+}
