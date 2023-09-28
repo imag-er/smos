80f6ca572d923ec07b575fa622a487880ec3e283
imag-er <2274265784@qq.com>
2023-09-28 23:38:42 +0800

    interrupt to be finish

---
+++ src/interrupts.rs
@@ -0,0 +1,7 @@
+use x86_64::structures::idt::InterruptDescriptorTable;
+use crate::println;
+
+pub fn init_idt() {
+	let mut idt = InterruptDescriptorTable::new();
+	// idt.breakpoint.set_handler_fn();
+}
