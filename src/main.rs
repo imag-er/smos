3673a99907ba91d000a7c27b57e84550d7d35759
imag-er <2274265784@qq.com>
2023-09-28 23:27:22 +0800

    print to buffer

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,21 @@
 #![no_main]
 
 mod vga_buffer;
-
+mod serial;
 
 use core::panic::PanicInfo;
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}",_info);
     loop {}
 }
 
 
+
 #[no_mangle]
 pub extern "C" fn _start() ->! {
+    println!("Hello\r123\n123");
 
-    vga_buffer::print_test_msg();
     loop{
 
     }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 
 
 
@@ -5,7 +6,7 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum Color {
-	Black = 0,
+    Black = 0,
     Blue = 1,
     Green = 2,
     Cyan = 3,
@@ -28,91 +29,117 @@ pub enum Color {
 struct ColorCode(u8);
 
 impl ColorCode {
-	fn new(foreground_color  : Color,background_color : Color) -> ColorCode {
-		ColorCode((background_color as u8) << 4 | (foreground_color as u8))
-
-	}
+    fn new(foreground_color: Color, background_color: Color) -> ColorCode {
+        ColorCode((background_color as u8) << 4 | (foreground_color as u8))
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(C)]
 struct ScreenChar {
-	ascii_character : u8,
-	color_code : ColorCode,
+    ascii_character: u8,
+    color_code: ColorCode,
 }
 
-const BUFFER_HEIGHT : usize = 25;
-const BUFFER_WIDTH : usize = 80;
-
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
 
 #[repr(transparent)]
-struct  Buffer {
-	// 二维数组定义
-	chars : [[ScreenChar; BUFFER_WIDTH];BUFFER_HEIGHT]
+struct Buffer {
+    // 二维数组定义
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
+
+// 定义writer并实现接口
 // 用于向buffer写入
 pub struct Writer {
-	row_position : usize,
-	colume_position : usize,
-	color_code: ColorCode,
-	// vga buffer全局有效
-	buffer: &'static mut Buffer,
+    row_position: usize,
+    colume_position: usize,
+    color_code: ColorCode,
+    // vga buffer全局有效
+    buffer: &'static mut Buffer,
 }
-
 
 impl Writer {
-	// 写入byte
-	pub fn write_byte(&mut self,byte: u8)
-	{
-		match byte {
-			b'\n' => self.new_line(),
-			b'\r' => self.colume_position = 0,
-			byte => {
-				if self.colume_position >= BUFFER_WIDTH{
-					// 超出行宽度 新行
-					self.new_line();
-				}
-				// let row = BUFFER_HEIGHT - 1;
-				let row = self.row_position;
+    // 写入byte
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            b'\r' => self.colume_position = 0,
+            byte => {
+                if self.colume_position >= BUFFER_WIDTH {
+                    // 超出行宽度 新行
+                    self.new_line();
+                }
+                // let row = BUFFER_HEIGHT - 1;
+                let row = self.row_position;
 
-				let col = self.colume_position;
+                let col = self.colume_position;
 
-				let color_code = self.color_code;
+                let color_code = self.color_code;
 
-				self.buffer.chars[row][col] = ScreenChar {
-					ascii_character : byte,
-					color_code
-				};
+                self.buffer.chars[row][col] = ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                };
 
-				self.colume_position += 1;
-			}
-		}
-	}
+                self.colume_position += 1;
+            }
+			
+        }
+    }
 
-	pub fn write_string(&mut self,s: &str) {
-		for byte in s.bytes() {
-			match byte {
-				// 在CodePage437定义内的字符
-				0x20..=0x7E | b'\n' | b'\r' => self.write_byte(byte),
-				// 其他字符 打印方块 0xfe
-				_ => self.write_byte(0xfe),
-			}
-		}
-	}
-	fn new_line(&mut self) {
-		self.row_position += 1;
-		self.colume_position = 0;
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // 在CodePage437定义内的字符
+                0x20..=0x7E | b'\n' | b'\r' => self.write_byte(byte),
+                // 其他字符 打印方块 0xfe
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+    fn new_line(&mut self) {
+        self.row_position += 1;
+        self.colume_position = 0;
+    }
+}
+
+impl fmt::Write for Writer {
+	fn write_str(&mut self, s: &str) -> fmt::Result {
+		self.write_string(s);
+		Ok(())
 	}
 }
-pub fn print_test_msg()
-{
-	let mut vga_buffer_writer = Writer {
+use lazy_static::lazy_static;
+use spin::Mutex;
+
+// lazy初始化writer 
+lazy_static!{
+	pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer{
 		colume_position: 0,
 		row_position: 0,
-		color_code: ColorCode::new(Color::White, Color::Pink),
-		buffer: unsafe { &mut *(0xb8000 as *mut Buffer)}
-	};
+		color_code: ColorCode::new(Color::White,Color::Black),
+		buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+	});
+}
 
-	vga_buffer_writer.write_string("Hello World\n123123");
+
+// 定义print macro
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
 }
